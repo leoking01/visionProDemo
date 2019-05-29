2b20c5fd5c0746bd112943318889be242f7a3fb3
leoking02@foxmail.com <leoking02@foxmail.com>
2019-05-29 17:00:59 +0800

    vp---正在做图像拼接---201905291700-----

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -34,7 +34,12 @@
             this.btn_edittoolblock = new System.Windows.Forms.Button();
             this.btn_Run = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.操作ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.学习roi设置方法ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.研究图像缝合ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.cogRecordDisplay1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // cogRecordDisplay1
@@ -44,13 +49,13 @@
             this.cogRecordDisplay1.ColorMapPredefined = Cognex.VisionPro.Display.CogDisplayColorMapPredefinedConstants.None;
             this.cogRecordDisplay1.ColorMapUpperClipColor = System.Drawing.Color.Black;
             this.cogRecordDisplay1.ColorMapUpperRoiLimit = 1D;
-            this.cogRecordDisplay1.Location = new System.Drawing.Point(12, 12);
+            this.cogRecordDisplay1.Location = new System.Drawing.Point(12, 27);
             this.cogRecordDisplay1.Margin = new System.Windows.Forms.Padding(2);
             this.cogRecordDisplay1.MouseWheelMode = Cognex.VisionPro.Display.CogDisplayMouseWheelModeConstants.Zoom1;
             this.cogRecordDisplay1.MouseWheelSensitivity = 1D;
             this.cogRecordDisplay1.Name = "cogRecordDisplay1";
             this.cogRecordDisplay1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("cogRecordDisplay1.OcxState")));
-            this.cogRecordDisplay1.Size = new System.Drawing.Size(1006, 693);
+            this.cogRecordDisplay1.Size = new System.Drawing.Size(1006, 678);
             this.cogRecordDisplay1.TabIndex = 0;
             // 
             // btn_openimage
@@ -94,6 +99,39 @@
             this.textBox1.Size = new System.Drawing.Size(64, 21);
             this.textBox1.TabIndex = 4;
             // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.操作ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(928, 25);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 操作ToolStripMenuItem
+            // 
+            this.操作ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.学习roi设置方法ToolStripMenuItem,
+            this.研究图像缝合ToolStripMenuItem});
+            this.操作ToolStripMenuItem.Name = "操作ToolStripMenuItem";
+            this.操作ToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
+            this.操作ToolStripMenuItem.Text = "操作";
+            // 
+            // 学习roi设置方法ToolStripMenuItem
+            // 
+            this.学习roi设置方法ToolStripMenuItem.Name = "学习roi设置方法ToolStripMenuItem";
+            this.学习roi设置方法ToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.学习roi设置方法ToolStripMenuItem.Text = "学习roi设置方法";
+            this.学习roi设置方法ToolStripMenuItem.Click += new System.EventHandler(this.学习roi设置方法ToolStripMenuItem_Click);
+            // 
+            // 研究图像缝合ToolStripMenuItem
+            // 
+            this.研究图像缝合ToolStripMenuItem.Name = "研究图像缝合ToolStripMenuItem";
+            this.研究图像缝合ToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.研究图像缝合ToolStripMenuItem.Text = "研究图像缝合";
+            this.研究图像缝合ToolStripMenuItem.Click += new System.EventHandler(this.研究图像缝合ToolStripMenuItem_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -104,11 +142,15 @@
             this.Controls.Add(this.btn_edittoolblock);
             this.Controls.Add(this.btn_openimage);
             this.Controls.Add(this.cogRecordDisplay1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "MainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.MainForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.cogRecordDisplay1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -121,6 +163,10 @@
         private System.Windows.Forms.Button btn_edittoolblock;
         private System.Windows.Forms.Button btn_Run;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 操作ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 学习roi设置方法ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 研究图像缝合ToolStripMenuItem;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -12,15 +12,20 @@ using Cognex.VisionPro.ImageFile;
 using Cognex.VisionPro.ToolBlock;
 using Cognex.VisionPro;
 
+
+ using Cognex.VisionPro;
+
 namespace VproTest
 {
     public partial class MainForm : Form
     {
-        CogToolBlock m_tb;
-        CogImage8Grey m_Image;
-        public MainForm()
+        CogToolBlock   m_tb;
+        CogImage8Grey  m_Image;
+        public MainForm(   )
         {
              InitializeComponent();
+
+
         }
 
         private void btn_openimage_Click(object sender, EventArgs e)
@@ -33,14 +38,25 @@ namespace VproTest
                 m_Image = (CogImage8Grey)imagefile[0];
                 cogRecordDisplay1.Image = imagefile[0];
                 cogRecordDisplay1.Fit();
-
             }
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
             m_tb = new CogToolBlock();
-            m_tb = (CogToolBlock)CogSerializer.LoadObjectFromFile(Application.StartupPath + "/../..\\demo.vpp");
+            string nameOfVpp = Application.StartupPath + "\\..\\..\\..\\demo.vpp";
+            m_tb = (CogToolBlock)CogSerializer.LoadObjectFromFile(nameOfVpp);
+
+
+
+
+            CogImageFile imagefile = new CogImageFile();
+            imagefile.Open(Application.StartupPath + "/../../../in/bracket_std.idb", CogImageFileModeConstants.Read);
+            //m_Image = 
+            CogToolCollection tbTc = m_tb.Tools;
+            //tbTc.
+            //((CogImageFileTool)tbTc["CogImageFileTool1"]).Name = "";
+            ((CogImageFileTool)tbTc["CogImageFileTool1"]).InputImage = (CogImage8Grey)imagefile[0];
         }
 
         private void btn_edittoolblock_Click(object sender, EventArgs e)
@@ -67,5 +83,158 @@ namespace VproTest
                 //  angle = angle * 180 / Math.PI;
             }
         }
+
+        private void 学习roi设置方法ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            //InitializeFifo();
+            MessageBox.Show("study  roi  visonpro . ");
+
+            if (false)
+            {
+                aaa();
+                bbb();  //  错误演示-- 未将对象引用设置到对象的实例” 
+            }
+        }
+
+
+
+#region  JJJ
+        private CogFrameGrabbers myFrameGrabbers;
+        private ICogFrameGrabber myFrameGrabber;
+        private ICogAcqFifo myAcqFifo;
+
+        private void InitializeFifo()
+        {
+            const string VIDEO_FORMAT = "Sony XC75 640x480";
+            ICogAcqROI ROIParams;
+
+            myFrameGrabbers = new CogFrameGrabbers();
+            myFrameGrabber = myFrameGrabbers[0];
+            myAcqFifo = myFrameGrabber.CreateAcqFifo(VIDEO_FORMAT,
+                Cognex.VisionPro.CogAcqFifoPixelFormatConstants.Format8Grey, 0, false);
+
+            ROIParams = myAcqFifo.OwnedROIParams;
+            if (ROIParams != null)
+                ROIParams.SetROIXYWidthHeight(10, 20, 300, 200);
+        }
+#endregion
+
+
+
+
+
+        public string[ ] c;
+
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+            aaa();
+            bbb();
+        }
+
+        private void aaa()
+        {
+            string[] c = new string[3];    //  错误演示-- 未将对象引用设置到对象的实例” 
+            //c = new string[3];    //  正确的做法 
+            c[0] = "我们";
+            c[1] = "西部";
+            c[2] = "学习";
+        }
+
+        private void bbb()
+        {
+            //System.Console.WriteLine(c[0] + c[1] + c[2]);
+
+            string resD = c[0] + c[1] + c[2];
+        }
+
+        private void 研究图像缝合ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+
+            CogImageFile imagefile_0 = new CogImageFile();
+            imagefile_0.Open(Application.StartupPath + "/../../../street/street_0.bmp", CogImageFileModeConstants.Read);
+            CogImage24PlanarColor image_24_0 = new CogImage24PlanarColor();
+            image_24_0 = (CogImage24PlanarColor)imagefile_0[0];
+            CogImage8Grey imageGray8_0 = CogImageConvert.GetIntensityImage(image_24_0, 0, 0, image_24_0.Width, image_24_0.Height);
+
+
+            CogImageFile imagefile_1 = new CogImageFile();
+            imagefile_1.Open(Application.StartupPath + "/../../../street/street_1.bmp", CogImageFileModeConstants.Read);
+            CogImage24PlanarColor image_24_1 = new CogImage24PlanarColor();
+            image_24_1 = (CogImage24PlanarColor)imagefile_1[0];
+            CogImage8Grey imageGray8_1 = CogImageConvert.GetIntensityImage(image_24_1, 0, 0, image_24_1.Width, image_24_1.Height);
+
+
+            CogImageFile imagefile_2 = new CogImageFile();
+            imagefile_2.Open(Application.StartupPath + "/../../../street/street_2.bmp", CogImageFileModeConstants.Read);
+            CogImage24PlanarColor image_24_2 = new CogImage24PlanarColor();
+            image_24_2 = (CogImage24PlanarColor)imagefile_2[0];
+            CogImage8Grey imageGray8_2 = CogImageConvert.GetIntensityImage(image_24_2, 0, 0, image_24_2.Width, image_24_2.Height);
+
+
+
+            // 灰度图像 
+            //CogImage8Grey image8 = (CogImage8Grey)m_tb.GetScriptTerminalData("imageInput");   
+            CogImage8Grey image8 = (CogImage8Grey)m_tb.GetScriptTerminalData("OutputImage");
+            image8 = imageGray8_0.Copy();
+            CogToolCollection tbTc = m_tb.Tools;
+            //tbTc.
+            if (false)
+            {
+                image8 = (CogImage8Grey)((CogImageFileTool)tbTc["CogImageFileTool1"]).OutputImage;
+            }
+           
+
+
+
+            CogImage8Grey image_002 = new CogImage8Grey();
+            //image_002 = image8;
+            image_002 = image8.Copy();
+            CogImage8Grey image_003 = image8.Copy();
+
+
+
+            // 将各个图像进行缝合组成一个完整的大图
+            // 灰度图像 
+            //CogIPOneImageTool imageCtrl_02 = (CogIPOneImageTool)mToolBlock.Tools["CogIPOneImageTool2"];
+            CogRectangle rt = new CogRectangle();
+            rt.SetCenterWidthHeight(0, 0, image8.Width * 3, image8.Height * 3);
+            CogImage8Grey m_gray = rt.CreateRLE(1, 0).CreateImage8Grey();
+            //imageCtrl_02.InputImage = m_gray;
+
+
+
+            //    CogImage8Grey inputImage = ( CogImage8Grey ) image8;
+            //    tb.SetScriptTerminalData("Input", inputImage);
+            CogImageStitch stt = new CogImageStitch();
+            CogTransform2DLinear rootFromBlending = new CogTransform2DLinear();
+            //缩放，旋转，平移
+            rootFromBlending.SetScalingsRotationsTranslation(    1,    1,    0,   0,    0   , 0   );
+            //    rootFromBlending.SetScalingsRotationTranslation();
+            stt.AllocateBlendingBuffer(   image8.Width *3 ,   image8.Height*3 ,   rootFromBlending   );
+            //stt.BlendImageIntoBuffer(image8, m_gray);
+            stt.BlendImageIntoBuffer( imageGray8_0, imageGray8_0, m_gray,           0,       0             );
+            stt.BlendImageIntoBuffer( imageGray8_1, imageGray8_1, m_gray, image8.Width,   image8.Height       );
+            stt.BlendImageIntoBuffer( imageGray8_2, imageGray8_2, m_gray, image8.Width*2, image8.Height*2    );
+
+
+
+
+
+
+
+
+            stt.FillDestinationImageFromBuffer(  m_gray   );
+            //    BlendImageIntoBuffer(  CogImage8Grey, CogImage8Grey, CogImage8Grey, Int32, Int32 )
+
+
+            cogRecordDisplay1.Image = m_gray;
+            cogRecordDisplay1.Fit();
+        }
+
+
+
+
     }
 }
